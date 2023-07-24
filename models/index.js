1e258dd4b2ee8692bc084bb3d9fe105cb900d2ec
rethissunstar <rethissunstar@gmail.com>
2023-07-24 16:16:11 -0400

    created config.json to be able to deploy

--- models/index.js
+++ models/index.js
@@ -1,19 +1,43 @@
-const User = require('./User');
-const Blog = require('./Blogs');
-//const Painting = require('./Painting');
+'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-User.hasMany(Blog, {
-  foreignKey: 'username',
-  as: 'blogs',
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-Blog.belongsTo(User, {
-  foreignKey: 'username',
-  as: 'user',
-});
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-// User.hasMany(Blog, { foreignKey: 'user_id', as: 'blogs' });
-// Blog.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
-
-module.exports = { User, Blog};
+module.exports = db;
