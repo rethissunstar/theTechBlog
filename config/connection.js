550a31f9d6942f0bb0188cea87718324166510ee
rethissunstar <rethissunstar@gmail.com>
2023-07-24 15:52:43 -0400

    heroku changes

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,14 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -14,4 +21,6 @@ if (process.env.JAWSDB_URL) {
     },
   );
 }
+
+
 module.exports = sequelize;
